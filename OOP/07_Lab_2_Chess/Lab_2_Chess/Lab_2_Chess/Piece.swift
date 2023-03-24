bcfdc4bbdd0ccf5c19a277fd22801ff05420cdf0
grigorybaluev <gr.baluev@gmail.com>
2023-03-24 14:45:08 -0400

    Add subclasses of Pieces with overriden description and move()

--- OOP/07_Lab_2_Chess/Lab_2_Chess/Lab_2_Chess/Piece.swift
+++ OOP/07_Lab_2_Chess/Lab_2_Chess/Lab_2_Chess/Piece.swift
@@ -8,6 +8,9 @@
 import Foundation
 
 class Piece: CustomStringConvertible {
+    var description: String {
+        return ""
+    }
     private var value: Int
     private (set) var isWhite: Bool
     
@@ -24,10 +27,73 @@ class Piece: CustomStringConvertible {
         value = newValue
     }
     
+    func move() -> Void {
+        return
+    }
 }
 
-extension Piece {
-    var description: String {
-        return ""
+class Pawn: Piece {
+    override var description: String {
+        return "Pawn(value=" + String(self.getValue()) + ")"
+    }
+    
+    override func move() {
+        print("Forward 1")
+        return
+    }
+}
+
+class Knight: Piece {
+    override var description: String {
+        return "Knight(value=" + String(self.getValue()) + ")"
+    }
+    
+    override func move() {
+        print("Like an L")
+        return
+    }
+}
+
+class Bishop: Piece {
+    override var description: String {
+        return "Bishop(value=" + String(self.getValue()) + ")"
+    }
+    
+    override func move() {
+        print("Diagonally")
+        return
+    }
+}
+
+class Rook: Piece {
+    override var description: String {
+        return "Pawn(value=" + String(self.getValue()) + ")"
+    }
+    
+    override func move() {
+        print("Horizontally or vertically")
+        return
+    }
+}
+
+class Queen: Piece {
+    override var description: String {
+        return "Queen(value=" + String(self.getValue()) + ")"
+    }
+    
+    override func move() {
+        print("Like bishop and rook")
+        return
+    }
+}
+
+class King: Piece {
+    override var description: String {
+        return "King(value=" + String(self.getValue()) + ")"
+    }
+    
+    override func move() {
+        print("One square")
+        return
     }
 }
