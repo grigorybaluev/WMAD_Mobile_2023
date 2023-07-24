6f41f0e227ce75d781ee1a9979164103e52df3ee
grigorybaluev <gr.baluev@gmail.com>
2023-07-24 11:58:00 -0400

    Add Stack and Queue

--- AGDS/Swift_AGDS/Swift_AGDS/main.swift
+++ AGDS/Swift_AGDS/Swift_AGDS/main.swift
@@ -7,15 +7,54 @@
 
 import Foundation
 
-print("Hello, World!")
+//print("Hello, World!")
 //
 //print(power(base: 2, exp: 6))
 
-print(evaluate("7+2"))
-print(evaluate("((7+2)+1)"))
+//print(evaluate("7+2"))
+//print(evaluate("((7+2)+1)"))
 
 
 //printDecimal(n: 5)
 
 
-permutations(str: "park")
+//permutations(str: "park")
+
+
+var nums = Bag<Int>()
+
+for i in 0..<10 {
+  nums.add(i)
+}
+
+for num in nums {
+  print(num)
+}
+
+print()
+
+var letters = Stack<String>()
+
+letters.push("A")
+letters.push("B")
+letters.push("C")
+print(letters.pop()!)
+print(letters.pop()!)
+print(letters.pop()!)
+
+print()
+
+var strings = Queue<String>()
+
+strings.enqueue("A")
+strings.enqueue("B")
+strings.enqueue("C")
+
+print(strings.peek()!)
+print(strings.dequeue()!)
+print(strings.peek()!)
+print(strings.dequeue()!)
+print(strings.peek()!)
+
+
+
